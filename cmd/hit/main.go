c5c9794657435a1a221eefb06e1bb5578d26237d
Chen Quan <chenquan@osai.club>
2020-07-07 18:14:30 +0800

    Update local

--- cmd/hit/main.go
+++ cmd/hit/main.go
@@ -26,7 +26,18 @@ import (
 	"strings"
 )
 
-var db = map[string]string{
+type ram map[string]string
+
+func (r ram) Get(key string) ([]byte, bool) {
+	if s, ok := r[key]; !ok {
+		return nil, ok
+	} else {
+		return []byte(s), ok
+	}
+
+}
+
+var db ram = map[string]string{
 	"Tom":      "630",
 	"Jack":     "589",
 	"Sam":      "567",
@@ -64,8 +75,9 @@ func createNewGroupDefault(name string) *hit.Group {
 	return hit.NewGroupDefault(name, 2<<10, hit.GetterFunc(
 		func(key string) ([]byte, error) {
 			log.Println("[SlowDB] search key", key)
-			if v, ok := db[key]; ok {
-				return []byte(v), nil
+
+			if v, ok := db.Get(key); ok {
+				return v, nil
 			}
 			return nil, fmt.Errorf("%s not exist", key)
 		}))
